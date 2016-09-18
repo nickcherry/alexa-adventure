100f5d2f302d57442274205daaecdbbc34c12e1f
Nick Cherry <ncherry@cipherhealth.com>
2016-09-18 13:26:24 -0400

    finish StateManager tests

--- apps/adventure/test/engine/state_manager_tests.js
+++ apps/adventure/test/engine/state_manager_tests.js
@@ -9,6 +9,7 @@ const expect = require('chai').expect;
 const sinonChai = require("sinon-chai");
 const stub = require('sinon').stub;
 
+const State = require('../../engine/state');
 const StateFactory = require('../factories/state_factory');
 const StateManager = require('../../engine/state_manager');
 const StateManagerFactory = require('../factories/state_manager_factory');
@@ -35,20 +36,33 @@ describe('StateManager', () => {
 
   describe('getState', () => {
     it('should forward getState requests to the external/db method', () => {
-      const stateManager = StateManagerFactory.default();
-      const getState = stub(stateManager, 'getState', () => Promise.resolve())
-      stateManager.getState('abc123');
-      expect(getState).to.have.been.calledWith('abc123');
+      const persistedState = StateFactory.default({ mapId: 'dungeon' });
+      const getState = stub().returns(Promise.resolve(persistedState));
+      const stateManager = StateManagerFactory.default({ getState });
+      return stateManager.getState('abc123').then((state) => {
+        expect(getState).to.have.been.calledWith('abc123');
+        expect(state).to.deep.eq(persistedState);
+      });
     });
-    it('should forward setState requests to the external/db method', () => {
-      const state = StateFactory.default({ mapId: 'dungeon' });
-      const stateManager = StateManagerFactory.default();
-      const setState = stub(stateManager, 'setState', () => Promise.resolve())
-      stateManager.setState('abc123', state);
-      expect(setState).to.have.been.calledWith('abc123', state);
-    });
-    xit('should return a new state when no previous state can be found', () => {
 
+    it('should return a new state when no previous state can be found', () => {
+      const getState = stub().returns(Promise.resolve(null));
+      const stateManager = StateManagerFactory.default({ getState });
+      return stateManager.getState('abc123').then((state) => {
+        expect(state).to.deep.eq(new State());
+      });
+    });
+  });
+
+  describe('setState', () => {
+    it('should forward setState requests to the external/db method', () => {
+      const newState = StateFactory.default({ mapId: 'dungeon' });
+      const setState = stub().returns(Promise.resolve(newState));
+      const stateManager = StateManagerFactory.default({ setState });
+      return stateManager.setState('abc123', newState).then((state) => {
+        expect(state).to.deep.eq(newState);
+        expect(setState).to.have.been.calledWith('abc123', newState);
+      });
     });
   });
 });
